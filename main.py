5db0601aee26ee9d7e9f33a6369588e16d9ff041
mazinhabis <mazin00@windowslive.com>
2022-12-19 09:49:12 +0300

    added the bikeshare python files, and created .gitignore for the csv files

---
+++ main.py
@@ -0,0 +1,19 @@
+from bikeshare import get_filters, load_data, time_stats, station_stats, trip_duration_stats, user_stats, display_data
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+        display_data(df)
+
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
